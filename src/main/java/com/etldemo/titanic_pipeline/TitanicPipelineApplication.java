6ded042f2b77b9f3c4da1fea423fc38ed710c1ba
venkata ganesh <chimatasaivenkataganesh@gmail.com>
2026-03-25 17:28:24 +0530

    chore: initial Spring Boot project scaffold

---
+++ src/main/java/com/etldemo/titanic_pipeline/TitanicPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.etldemo.titanic_pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TitanicPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TitanicPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/etldemo/titanic_pipeline/TitanicPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etldemo.titanic_pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TitanicPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
